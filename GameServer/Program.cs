934f6c5a10ab04f8fa7c5eed6803127cca9c0f14
Alpaca96 <jose20050428@gmail.com>
2026-07-24 13:06:19 +0200

    Algunas cambios

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -12,33 +12,29 @@ public class PlayerServerState
     public float PosX;
     public float PosY;
     public uint UltimoTickRecibido;
+    
+    // Almacenamos el input para procesarlo luego en el Tick
+    public float CurrentInputX;
+    public float CurrentInputY;
 }
-
 
 class Program
 {
-
     static Dictionary<int, PlayerServerState> connectedPlayers = new Dictionary<int, PlayerServerState>();
-   
-
     static NetPacketProcessor packetProcessor = new NetPacketProcessor();
     static NetDataWriter writer = new NetDataWriter();
 
     //tick stuff
     static uint currentTick = 0;
     const float TICK_RATE = 30f;
-    const float TIME_PER_TICK = 1f / TICK_RATE;
+    const float TIME_PER_TICK = 1f / TICK_RATE; // = 0.0333f
 
     static void Main(string[] args)
     {
         EventBasedNetListener listener = new EventBasedNetListener();
         NetManager server = new NetManager(listener);
 
-
-
-        //listeners
-
-        // try conn, credentials
+        // LISTENERS
         listener.ConnectionRequestEvent += request =>
         {
             Console.WriteLine($"Conexion entrante de: {request.RemoteEndPoint}");
@@ -49,7 +45,6 @@ class Program
         listener.PeerConnectedEvent += peer =>
         {
             Console.WriteLine($"Jugador conectado ID asignado: {peer.Id}");
-
             PlayerServerState newPlayer = new PlayerServerState
             {
                 Id = peer.Id,
@@ -57,8 +52,7 @@ class Program
                 PosY = 0f, // spawnea en y 0
                 UltimoTickRecibido = 0
             };
-
-            connectedPlayers.Add(peer.Id,newPlayer); //list it
+            connectedPlayers.Add(peer.Id, newPlayer); //list it
 
             // send his id to the client
             WelcomePacket welcome = new WelcomePacket { MyId = peer.Id };
@@ -71,57 +65,42 @@ class Program
         {
             Console.WriteLine("Jugador " + peer.Id + " se ha desconectado");
             connectedPlayers.Remove(peer.Id);
-
         };
 
-        listener.NetworkReceiveEvent += (peer, reader,channel, deliveryMethod) =>
+        listener.NetworkReceiveEvent += (peer, reader, channel, deliveryMethod) =>
         {
             //process packets
             try
             {
                 packetProcessor.ReadAllPackets(reader, peer);
             }
-            catch (Exception ex) {
+            catch (Exception ex) 
+            {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"\n--- ERROR AL RECIBIR PAQUETE ---");
                 Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
                 Console.ResetColor();
-
             }
-            };
+        };
 
         packetProcessor.SubscribeReusable<PlayerInputPacket, NetPeer>((packet, peer) =>
         {
-           
-            // get player
             if (connectedPlayers.TryGetValue(peer.Id, out PlayerServerState jugador))
             {
-
-                // old tick = discarded
                 if (packet.Tick >= jugador.UltimoTickRecibido)
                 {
-                    
                     jugador.UltimoTickRecibido = packet.Tick;
-
-                    // apply movement (5f speed)
-                    // TODO multiply with deltaTime
-                    float velocidad = 5f * 0.015f; // 0.015 sleep thread
-
-                    jugador.PosX += packet.InputX * velocidad;
-                    jugador.PosY += packet.InputY * velocidad;
+                    jugador.CurrentInputX = packet.InputX;
+                    jugador.CurrentInputY = packet.InputY;
 
                     Console.WriteLine($"Jugador {peer.Id} movido a X:{jugador.PosX} Y:{jugador.PosY}");
                 }
             }
         });
 
-        //endlisteners
-
         // port start
         server.Start(9050);
         Console.WriteLine("Servidor iniciado | Esperando jugadores...");
-
 
         Stopwatch timer = new Stopwatch();
         timer.Start();
@@ -133,20 +112,19 @@ class Program
             //real time since last tick
             double deltaTime = timer.Elapsed.TotalSeconds;
             timer.Restart();
-
             acum += deltaTime;
 
             //get incoming net packets
-            server.PollEvents(); // `process
+            server.PollEvents(); // process
 
             while (acum >= TIME_PER_TICK)
             {
                 // we ticked
                 currentTick++;
-
-
+                
+                SimulateWorld();
                 UpdateClients(server);
-
+                
                 acum -= TIME_PER_TICK;
             }
 
@@ -158,13 +136,32 @@ class Program
         Console.ReadLine();
     }
 
+    // Simulamos todo el mundo (por ahora solo los jugadores)
+    static void SimulateWorld()
+        {
+            foreach (var player in connectedPlayers.Values)
+            {
+                float velocidad = 5f; 
+                
+                player.PosX += player.CurrentInputX * velocidad * TIME_PER_TICK;
+                player.PosY += player.CurrentInputY * velocidad * TIME_PER_TICK;
+                
+                // Si no hemos recibido ningun paquete del jugador en 15 ticks (medio segundo), entonces asumimos
+                // que se ha desconectado o lo que sea, reiniciamos el input 0 para no seguir usando el último input recivido
+                if (currentTick - player.UltimoTickRecibido > 15)
+                {
+                    player.CurrentInputX = 0f;
+                    player.CurrentInputY = 0f;
+                }
+            }
+        }
+
     static void UpdateClients(NetManager server)
     {
-
         if (connectedPlayers.Count == 0) return;
 
-        foreach (var player in connectedPlayers.Values) {
-
+        foreach (var player in connectedPlayers.Values) 
+        {
             PlayerStatePacket state = new PlayerStatePacket
             {
                 PlayerId = player.Id,
@@ -174,13 +171,8 @@ class Program
             };
 
             writer.Reset();
-
             packetProcessor.Write(writer, state);
-
             server.SendToAll(writer, DeliveryMethod.Sequenced);
-
         }
-
     }
 }
-
